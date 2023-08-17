30a905fdb263521f98e4c0c89a9632792bad4c24
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2023-08-17 12:53:54 +0200

    changes

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,47 +2,57 @@ import {
 	Center,
 	ContactShadows,
 	Environment,
+	Float,
 	Html,
 	Text,
 } from "@react-three/drei";
 import Iphone from "./Iphone";
+import { useThree } from "@react-three/fiber";
 
 export default function Experience() {
+	const viewport = useThree((state) => state.viewport);
 	return (
 		<>
-			<color args={["navajoWhite"]} attach="background" />
+			<color args={["#CCD1F6"]} attach="background" />
 
 			<Environment preset="city" />
 
-			<Center>
-				<Iphone
+			<Center
+				position={[0, -0.2, 0]}
+				onCentered={({ container, height }) =>
+					container.scale.setScalar(viewport.height / height)
+				}
+			>
+				<Float rotationIntensity={0.05}>
+					<Iphone
 
-				// position-x={-0.5}
-				// position-z={-0.5}
-				/>
+					// position-x={-0.5}
+					// position-z={-0.5}
+					/>
 
-				<Text
-					font="./Brocklyns.woff"
-					fontSize={1}
-					position={[3, 2, 0.75]}
-					rotation-y={-1.25}
-					color={"green"}
-					maxWidth={2}
-				>
-					gbp
-				</Text>
-				<Html
-					// wrapperClass="htmlScreen"
-					distanceFactor={5}
-					position={[3, 0, 0.75]}
-					// rotation-x={-0.256}
-				>
-					<span class="scroll-btn">
-						<span class="mouse">
-							<span></span>
+					<Text
+						font="./Brocklyns.woff"
+						fontSize={1}
+						position={[3, 1.2, 0.75]}
+						rotation-y={-1.25}
+						color={"green"}
+						maxWidth={2}
+					>
+						gbp
+					</Text>
+					<Html
+						// wrapperClass="htmlScreen"
+						distanceFactor={5}
+						position={[3, 0, 0.75]}
+						// rotation-x={-0.256}
+					>
+						<span class="scroll-btn">
+							<span class="mouse">
+								<span></span>
+							</span>
 						</span>
-					</span>
-				</Html>
+					</Html>
+				</Float>
 			</Center>
 
 			<ContactShadows position-y={-1.4} opacity={3} scale={5} blur={2.4} />
--- src/Iphone.jsx
+++ src/Iphone.jsx
@@ -1,23 +1,23 @@
-import { Float, Html, useGLTF, useHelper } from "@react-three/drei";
+import { Float, Html, Mask, useGLTF, useHelper } from "@react-three/drei";
 
-import React, { useRef } from "react";
+import React from "react";
 
 function Iphone(props) {
-	const { scene } = useGLTF(
+	const { scene, nodes, materials } = useGLTF(
 		"https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf"
 	);
 
 	return (
 		// <Float rotationIntensity={0.1}>
 		<primitive
-			scale={1.1}
 			rotation-x={-0.5}
 			rotation-z={-0.2}
 			rotation-y={-0.1}
+			position-y={-0.9}
 			object={scene}
 			{...props}
 		>
-			<Html
+			{/* <Html
 				// scale={0.99}
 				transform
 				wrapperClass="htmlScreen"
@@ -26,7 +26,29 @@ function Iphone(props) {
 				// rotation-x={-0.256}
 			>
 				<iframe src="https://www.lipsum.com/" />
-			</Html>
+			</Html> */}
+
+			<Mask
+				id={1}
+				colorWrite={false}
+				depthWrite={false}
+				geometry={nodes.SCREEN.geometry}
+				castShadow
+				receiveShadow
+			>
+				{/* We can drop the HTML inside, make it a 3d-transform and portal it to the dom container above */}
+				<Html
+					// scale={0.99}
+					transform
+					wrapperClass="htmlScreen"
+					distanceFactor={1}
+					position={[0.52, 1.65, -1]}
+					// rotation-x={-0.256}
+				>
+					<iframe src="https://bruno-simon.com/html/" />
+					{/* <iframe src="https://landing-page-lac-theta.vercel.app/" /> */}
+				</Html>
+			</Mask>
 		</primitive>
 		// </Float>
 	);
